7fb664262347395aaecf250a61ecf1263d2e0068
Pedro Santana <plucascs580@gmail.com>
2023-09-27 21:02:18 -0300

    Commit de teste. Configurando modulos intellij

---
+++ Desafios/Livraria Virtual/src/Impresso.java
@@ -0,0 +1,42 @@
+public class Impresso extends Livro{
+	
+	float frete;
+	int estoque;
+	
+	Impresso(){
+	}
+	
+	Impresso(String titulo, String autores, String editora, float preco, float frete, int estoque){
+		super(titulo, autores, editora, preco);
+		this.frete = frete;
+		this.estoque = estoque;
+	}
+
+	public float getFrete() {
+		return frete;
+	}
+
+	public void setFrete(float frete) {
+		this.frete = frete;
+	}
+
+	public int getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(int estoque) {
+		this.estoque = estoque;
+	}
+	
+	public void atualizarEstoque() {
+		this.estoque --;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString() + "Frete: " + frete + "\n"
+				+ "Estoque: " + estoque;
+	}
+	
+
+}
---
+++ Desafios/Livraria Virtual/src/Livro.java
@@ -0,0 +1,58 @@
+public abstract class Livro {
+
+	String titulo;
+	String autores;
+	String editora;
+	float preco;
+	
+	Livro(){
+	}
+	
+	Livro(String titulo, String autores, String editora, float preco){
+		this.titulo = titulo;
+		this.autores = autores;
+		this.editora = editora;
+		this.preco = preco;
+	}
+	
+	public String getTitulo() {
+		return this.titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getAutores() {
+		return autores;
+	}
+
+	public void setAutores(String autores) {
+		this.autores = autores;
+	}
+
+	public String getEditora() {
+		return editora;
+	}
+
+	public void setEditora(String editora) {
+		this.editora = editora;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	
+	public String toString() {
+		return 
+		"Titulo do livro: " + this.titulo + "\n"
+		+ "Autor do livro: " + this.autores + "\n"
+		+ "Editora do livro: " + this.editora + "\n"
+		+ "Preço do livro: " + this.preco + "\n";
+	}
+	
+}
