60adcfedea1fdcca3fca39e820d3b0fe50edf754
Fábio Carvalho <fabiodeveloper1@gmail.com>
2023-10-08 23:33:56 -0300

    Atualizando projeto

---
+++ Projeto/LocadoraCarros/src/main/java/dados/test/ListaTodosMotoristas.java
@@ -0,0 +1,17 @@
+package test;
+
+import dados.DadosMotorista;
+
+public class ListaTodosMotoristas {
+
+	public static void main(String[] args) {
+
+		DadosMotorista motList = new DadosMotorista();
+
+		System.out.println("---Lista Motoristas---");
+
+		motList.listaMotoristas();
+
+	}
+
+}
